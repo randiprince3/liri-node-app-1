b72ed25524d2b42bebdb5752ef1cae72f8f4bdd1
ykeanu <ykhernan@ucla.edu>
2017-10-29 18:50:11 -0700

    Basic functionality

---
+++ keys.js
@@ -0,0 +1,7 @@
+// Twitter API keys
+exports.twitterKeys = {
+  consumer_key: 'exlLbDATOZshAkUOLi4aAM0Pl',
+  consumer_secret: 'gOSvZIS0oR75SQk9mQQWWqX9M2SkaGxi6qua5RwVlvhC8MpGk6',
+  access_token_key: '924804566758985728-oZsPVuyzc5B8DkMIeZ7P8MaqIPoWuxl',
+  access_token_secret: 'PYPILteDpn5SOJVdfeStNhx9TLAaq9xHSn88wDhrJiN20',
+}
